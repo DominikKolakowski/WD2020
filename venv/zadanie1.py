263886f75a1c338c6d5b896e8a8b37f91b90dafe
local <local@WMII-V7USKCH2QG>
2020-03-06 19:07:49 +0100

    ll

---
+++ venv/zadanie1.py
@@ -0,0 +1,18 @@
+# a=5
+# b=7
+# c=4
+#
+# dzielenie1=a/c
+# dzielenie2=b/a
+# print("Float",dzielenie1 , dzielenie2)
+# print("Dzielenie calkowite",int(dzielenie2),int(dzielenie1))
+# reszta1=a%c
+# reszta2=b%c
+# print("Reszta z dzielenia ",reszta1,reszta2)
+# potega = a**b
+# print("Potęga",potega)
+# potega = pow(a,b)
+# print("Potęga",potega)
+
+
+
